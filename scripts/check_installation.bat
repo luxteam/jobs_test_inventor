8aeab99abd74696fafc7c167e3fe84d747052155
Ilia Nemankov <INemankov@luxoft.com>
2021-03-19 20:01:02 +0300

    Implement draft script to check installation (except render and saving of results)

--- scripts/check_installation.bat
+++ scripts/check_installation.bat
@@ -3,4 +3,4 @@ if not defined TOOL_VERSION set TOOL_VERSION=2022
 set OUTPUT_PATH=%2
 if not defined OUTPUT_PATH set OUTPUT_PATH="..\\installation_check"
 
-python check_installation.py --tool "C:\Program Files\Autodesk\Inventor %TOOL%\Bin\Inventor.exe" --output_path %OUTPUT_PATH%
+python check_installation.py --tool "C:\Program Files\Autodesk\Inventor %TOOL_VERSION%\Bin\Inventor.exe" --output_path %OUTPUT_PATH% --assets_path "C:\\TestResources\\usd_inventor_autotests_assets"
--- scripts/check_installation.py
+++ scripts/check_installation.py
@@ -2,44 +2,142 @@ import argparse
 import os
 import subprocess
 import win32gui
+import win32api
+import pyautogui
 from time import sleep
 from datetime import datetime
+from subprocess import Popen, PIPE
+import traceback
+import pyscreenshot
+
+
+def close_process(process):
+    Popen("taskkill /F /PID {pid} /T".format(pid=process.pid))
+
+
+def make_screen(screen_path):
+    screen = pyscreenshot.grab()
+    screen = screen.convert('RGB')
+    screen.save(screen_path)
+
+
+def moveTo(x, y):
+    print("Move to x = {}, y = {}".format(x, y))
+    pyautogui.moveTo(x, y)
+
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    process = None
+    rc = 0
 
-    parser.add_argument('--tool', required=True)
-    parser.add_argument('--output_path', required=True)
+    try:
+        parser = argparse.ArgumentParser()
 
-    args = parser.parse_args()
+        parser.add_argument('--tool', required=True)
+        parser.add_argument('--output_path', required=True)
+        parser.add_argument('--assets_path', required=True)
 
-    if not os.path.exists(baseline_path):
-        os.makedirs(baseline_path)
+        args = parser.parse_args()
 
-    print("Open Inventor")
+        if not os.path.exists(args.output_path):
+            os.makedirs(args.output_path)
 
-    process = subprocess.Popen(cmdScriptPath, shell=True,
-                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print("Open Inventor")
 
-    inventor_window = None
+        process = Popen(args.tool, shell=True, stdout=PIPE, stderr=PIPE)
 
-    start_time = datetime.now()
-    # Wait a minute to open Inventor
-    while not inventor_window and (datetime.now() - start_time).total_seconds() <= 60:
-        inventor_window = win32gui.FindWindow("Autodesk Inventor Professional 2022", None)
-        print(str(inventor_window))
-        sleep(5)
+        inventor_window = None
 
-    if not inventor_window:
-        print("Inventor window wasn't found")
-        process.terminate()
-        sleep(10)
-        process.kill()
-        exit(-1)
-    else:
-        print("Inventor window found")
+        start_time = datetime.now()
+        # Wait a minute to open Inventor
+        while not inventor_window and (datetime.now() - start_time).total_seconds() <= 60:
+            inventor_window = win32gui.FindWindow(None, "Autodesk Inventor Professional 2022")
+            sleep(5)
 
-    process.terminate()
-    sleep(10)
-    process.kill()
+        if not inventor_window:
+            print("Inventor window wasn't found")
+            rc = -1
+        else:
+            print("Inventor window found. Wait a bit")
+            # TODO check window is ready by window content
+            sleep(3)
+            make_screen(os.path.join(args.output_path, "0_opened_inventor.jpg"))
+
+            print("Screen resolution: width = {}, height = {}".format(win32api.GetSystemMetrics(0), win32api.GetSystemMetrics(1)))
+            inventor_window_rect = win32gui.GetWindowRect(inventor_window)
+            print("Left-top corner position: x = {}, y = {}".format(inventor_window_rect[0], inventor_window_rect[1]))
+
+            # Open "File" tab
+            file_tab_x = inventor_window_rect[0] + 30
+            file_tab_y = inventor_window_rect[1] + 45
+            moveTo(file_tab_x, file_tab_y)
+            sleep(1)
+            make_screen(os.path.join(args.output_path, "1_before_choose_scene.jpg"))
+            pyautogui.click()
+            sleep(1)
+
+            # Click "Open file" button
+            open_file_button_x = inventor_window_rect[0] + 75
+            open_file_button_y = inventor_window_rect[1] + 170
+            moveTo(open_file_button_x, open_file_button_y)
+            sleep(1)
+            pyautogui.click()
+            sleep(1)
+            make_screen(os.path.join(args.output_path, "2_choose_scene.jpg"))
+
+            # Set scene path
+            print("Bottom edge position: y = {}".format(inventor_window_rect[3]))
+            scene_name_field_x = (int)((inventor_window_rect[2] - inventor_window_rect[0]) / 2)
+            scene_name_field_y = inventor_window_rect[3] - 175
+            moveTo(scene_name_field_x, scene_name_field_y)
+            sleep(1)
+            pyautogui.click(clicks=2)
+            sleep(1)
+            scene_path = os.path.abspath(os.path.join(args.assets_path, "Basic", "test_scene.iam"))
+            print("Scene path: {}".format(scene_path))
+            pyautogui.press("backspace")
+            sleep(1)
+            pyautogui.typewrite(scene_path)
+            sleep(2)
+
+            # Click "Open" button
+            print("Bottom-right corner position: x = {}, y = {}".format(inventor_window_rect[2], inventor_window_rect[3]))
+            open_button_x = inventor_window_rect[2] - 150
+            open_button_y = inventor_window_rect[3] - 45
+            moveTo(open_button_x, open_button_y)
+            sleep(1)
+            make_screen(os.path.join(args.output_path, "3_scene_path.jpg"))
+            pyautogui.click()
+            sleep(1)
+
+            # Wait scene opening
+            # TODO check that scene is opened by window content
+            sleep(10)
+            make_screen(os.path.join(args.output_path, "4_opened_scene.jpg"))
+
+            # Open "Tools" tab
+            tools_tab_x = inventor_window_rect[0] + 460
+            tools_tab_y = inventor_window_rect[1] + 45
+            moveTo(tools_tab_x, tools_tab_y)
+            sleep(1)
+            pyautogui.click()
+            sleep(1)
+
+            # Open USD Viewer
+            tools_tab_x = inventor_window_rect[0] + 965
+            tools_tab_y = inventor_window_rect[1] + 100
+            moveTo(tools_tab_x, tools_tab_y)
+            sleep(1)
+            pyautogui.click()
+            make_screen(os.path.join(args.output_path, "5_before_usd_viewer.jpg"))
+            sleep(1)
+
+            sleep(10)
+
+    except Exception as e:
+        print("Failed to check installation: {}".format(str(e)))
+        print("Traceback: {}".format(traceback.format_exc()))
+    finally:
+        if process:
+            close_process(process)
