ec2c982e290caf56929c62031ebcf7911e07a670
Ilia Nemankov <INemankov@luxoft.com>
2021-03-23 10:30:00 +0300

    Rename cache building script

---
+++ scripts/build_usd_cache.bat
@@ -0,0 +1,6 @@
+set TOOL_VERSION=%1
+if not defined TOOL_VERSION set TOOL_VERSION=2022
+set OUTPUT_PATH=%2
+if not defined OUTPUT_PATH set OUTPUT_PATH="..\\cache_building_results"
+
+python build_usd_cache.py --tool "C:\Program Files\Autodesk\Inventor %TOOL_VERSION%\Bin\Inventor.exe" --output_path %OUTPUT_PATH% --assets_path "C:\\TestResources\\usd_inventor_autotests_assets"
