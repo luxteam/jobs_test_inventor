806ae44e39584ebd28df962c3ceee47305b68786
Ilia Nemankov <INemankov@luxoft.com>
2021-04-04 11:59:43 +0300

    Fix waiting of USD Viewer in build_usd_cache script

--- scripts/build_usd_cache.py
+++ scripts/build_usd_cache.py
@@ -76,7 +76,7 @@ if __name__ == '__main__':
 
         start_time = datetime.now()
         # Wait a minute to open Inventor
-        while not inventor_window and (datetime.now() - start_time).total_seconds() <= 60:
+        while not inventor_window and (datetime.now() - start_time).total_seconds() <= 90:
             inventor_window = win32gui.FindWindow(None, "{}".format(args.tool_name))
             sleep(1)
 
