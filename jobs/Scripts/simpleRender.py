fc0d301f0f4d5871619dc72295c09ca3ce7aebbc
Ilia Nemankov <INemankov@luxoft.com>
2021-04-08 19:51:36 +0300

    Fix 005 case

--- jobs/Scripts/simpleRender.py
+++ jobs/Scripts/simpleRender.py
@@ -179,7 +179,7 @@ def execute_tests(args, current_conf):
                 utils.case_logger.info("Screen resolution: width = {}, height = {}".format(win32api.GetSystemMetrics(0), win32api.GetSystemMetrics(1)))
                 utils.case_logger.info("Open Inventor")
 
-                process = Popen(args.tool, shell=True, stdout=PIPE, stderr=PIPE)
+                process = Popen(args.tool)
 
                 inventor_window = utils.find_inventor_window(args)
 
